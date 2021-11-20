b0a049189cd622ec4085662cbc1e387ba7eea8fe
Maksim M <Mishcenkomv@gmail.com>
2021-11-20 12:04:02 -0800

    task 19_4_StringBuilder done

--- src/main/java/Task_19_4_StringBuilder.java
+++ src/main/java/Task_19_4_StringBuilder.java
@@ -1,4 +1,10 @@
 public class Task_19_4_StringBuilder {
+    //Написать метод, который принимает на вход строку букв,  и  возвращает строку, в которой только уникальные буквы
+    //
+    //Пример:
+    //method(“abcaczdbdakcazdbaz”)
+    // Вернуть
+    //“abczdk"
 
     private String returnUniqueLetters (String str) {
         StringBuilder referenceString = new StringBuilder();
