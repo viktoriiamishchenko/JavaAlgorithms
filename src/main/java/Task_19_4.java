ed07ec7b5fdaaa957b5b63445a84ff36c1f6c84f
Maksim M <Mishcenkomv@gmail.com>
2021-11-20 11:50:02 -0800

    task 19_4 done

---
+++ src/main/java/Task_19_4.java
@@ -0,0 +1,30 @@
+public class Task_19_4 {
+    //Написать метод, который принимает на вход строку букв,  и  возвращает строку, в которой только уникальные буквы
+    //
+    //Пример:
+    //method(“abcaczdbdakcazdbaz”)
+    // Вернуть
+    //“abczdk"
+
+    private String getUniqueLetter (String str) {
+        if (str.length() == 0) return null;
+
+        String result = String.valueOf(str.charAt(0));
+
+        for (int i = 0; i < result.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < result.length(); j++) {
+                if (str.charAt(i) == result.charAt(i)) {
+                    count++;
+                }
+            }
+           if (count == 0) {
+               result += str.charAt(i);
+           }
+        }
+
+        return result;
+    }
+
+
+}
