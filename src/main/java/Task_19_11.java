aa4416d963f8900f2b2947985eacd28a4a7454d3
Maksim M <Mishcenkomv@gmail.com>
2021-11-21 11:36:01 -0800

    Task_19_11 done

--- src/main/java/Task_19_11.java
+++ src/main/java/Task_19_11.java
@@ -9,28 +9,37 @@ public class Task_19_11 {
     //method("O, Draconian devil!", "!Leonardo Da Vinci,")  -> true
     //method("Oh, lame saint!", "The Mona Lisa,!") -> true
 
-    public static boolean idAnagram (String one, String two) {
-        boolean result = true;
-        one = one.toLowerCase(Locale.ROOT);
-        two = two.toLowerCase(Locale.ROOT);
+    public static boolean isAnagram (String one, String two) {
+        one = one.toLowerCase(Locale.ROOT).trim().replaceAll(" ", "");
+        two = two.toLowerCase(Locale.ROOT).trim().replaceAll(" ", "");
+        char [] arr1 = new char[one.length() + 1];
+        char [] arr2 = new char[two.length() + 1];
         int count = 0;
 
-        if (one.length() == two.length()) {
-            for (int i = 0; i < one.length() - 1; i++) {
-                for (int j = 0; j < two.length() - 1; j++) {
-                    if (one.charAt(i) == two.charAt(j)) {
-                        count ++;
-                    }
-                    if (count == one.length()) {
-                        result = true;
-                    }
-                }
-            }
-        } else {
-            result = false;
+        for (int i = 0; i < one.length(); i++) {
+            arr1[i] = one.charAt(i);
         }
 
-        return result;
+        for (int i = 0; i < two.length(); i++) {
+            arr2[i] = two.charAt(i);
+        }
+
+        if (one.length() == two.length()) {
+            for (int i = 0; i < arr1.length - 1; i++ ) {
+               for (int j = 0; j < arr2.length - 1; j++) {
+                   if(arr1[i] == arr2[j]) {
+                      count++;
+                      arr2[j] = ' ';
+                      break;
+                   }
+               }
+            }
+            if (one.length() == count) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- src/test/java/Task_19_11_Test.java
+++ src/test/java/Task_19_11_Test.java
@@ -9,10 +9,13 @@ public class Task_19_11_Test {
     public void testIsAnagram () {
 
         boolean result;
-        result = Task_19_11.idAnagram("O, Draconian devil!", "!Leonardo Da Vinci,");
+        result = Task_19_11.isAnagram("O, Dro", "Dor, O");
         Assert.assertTrue(result);
 
-        result = Task_19_11.idAnagram("Oh, lame saint!", "The Mona Lisa,!");
+        result = Task_19_11.isAnagram("O, Draconian devil!", "!Leonardo Da Vinci,");
+        Assert.assertTrue(result);
+
+        result = Task_19_11.isAnagram("Oh, lame saint!", "The Mona Lisa,!");
         Assert.assertTrue(result);
 
     }
